b97073b68120db887ebbda7541ae21b46c640f69
xslegendaxs <axenti.07@mail.ru>
2024-03-05 21:43:33 +0300

    1

--- positiveАrray.js
+++ positiveАrray.js
@@ -1,6 +1,8 @@
-let arr = [-1, -2, 0, 4, -23, 45];
+let arr = [-1, -2, 0, 4, -23, 45];   // массив
 
 function Numbers(arr) {
+    //использую команду filter() которая отфильтрует мне отрицательные числа от положительных 
+    //делаю чтобы число было больше 0 
     return arr.filter(num => num > 0);
 }
 const positiveNumbers = Numbers(arr);
--- theSameNumbersInTheArray.js
+++ theSameNumbersInTheArray.js
@@ -1,9 +1,11 @@
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
+const arr1 = [1, 2, 3]; //1 массив
+const arr2 = [4, 5, 6]; //2 массив
 const comparison = arr(arr1, arr2);
 
 function arr(arr1, arr2) {
-    const num1 = arr1.join('');
+    //просматриваю масивы как строку,если есть одинаковые сивмволы выдаю true
+    //если таких сивмволов нет выдаю false
+    const num1 = arr1.join(''); 
     const num2 = arr2.join('');
     for (let i of num1) {
         if (num2.includes(i)) {
@@ -12,4 +14,4 @@ function arr(arr1, arr2) {
     }
     return false;
 }
-console.log(comparison);
+console.log(comparison); 
